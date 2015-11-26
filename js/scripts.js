2dd222fbec20cf9550ae519086531442ae7da6d5
Joshua Carrafa <carrafa@gmail.com>
2015-11-26 14:37:46 -0500

    reformats

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,23 @@ var currentLevel = 0;
 
 currentBoard = [];
 
-var levels = [
-  {x: 355, y: 365},
-  {x: 12, y: 13},
-  {x: 250, y: 200}
-];
+var levels = [{
+  x: 355,
+  y: 365
+}, {
+  x: 12,
+  y: 13
+}, {
+  x: 250,
+  y: 200
+}];
 
-function levelSwitcher(){
-	if (rangeDetector(guy.x,guy.y,stairs.x,stairs.y, 10)===true){
-		currentLevel++;
-		}
-	if (currentLevel>=levels.length){
-				currentLevel = 0;
+function levelSwitcher() {
+  if (rangeDetector(guy.x, guy.y, stairs.x, stairs.y, 10) === true) {
+    currentLevel++;
+  }
+  if (currentLevel >= levels.length) {
+    currentLevel = 0;
   };
   stairs.x = levels[currentLevel].x;
   stairs.y = levels[currentLevel].y;
@@ -35,10 +40,11 @@ function levelSwitcher(){
 
 // global counter for animating
 
-var n  = 0;
-function updateN(){
+var n = 0;
+
+function updateN() {
   n++;
-  if(n>100){
+  if (n > 100) {
     n = 0;
   };
 };
@@ -72,15 +78,14 @@ document.getElementById('canvas-container').appendChild(fogCanvas);
 
 //=============object detection===================
 
-function rangeDetector(firstX,firstY,secondX,secondY, distance){
-  if(distance === undefined){
+function rangeDetector(firstX, firstY, secondX, secondY, distance) {
+  if (distance === undefined) {
     distance = 20
   };
-  if(
-    (firstX<secondX+distance && secondX-distance < firstX)
-    &&
-    (firstY<secondY+distance && secondY-distance <firstY)
-  ){
+  if (
+    (firstX < secondX + distance && secondX - distance < firstX) &&
+    (firstY < secondY + distance && secondY - distance < firstY)
+  ) {
     return true;
   } else {
     return false;
@@ -94,78 +99,77 @@ function rangeDetector(firstX,firstY,secondX,secondY, distance){
 
 //============ wall detection =====================
 
-collision = function(direction){
-		if(direction==='right'){
-			clipWidth = 2;
-			clipHeight = 10;
-			clipOffsetX = 30;
-			clipOffsetY = 0;
-		}
-		if(direction==='left'){
-			clipWidth = 2;
-			clipHeight = 10;
-			clipOffsetX = 0;
-			clipOffsetY = 0;
-		}
-		if(direction==='up'){
-			clipWidth = 10;
-			clipHeight = 2;
-			clipOffsetX = 0;
-			clipOffsetY = 0;
-		}
-		if(direction==='down'){
-			clipWidth = 10;
-			clipHeight = 2;
-			clipOffsetX = 0;
-			clipOffsetY = 30;
-		}
+collision = function(direction) {
+  if (direction === 'right') {
+    clipWidth = 2;
+    clipHeight = 10;
+    clipOffsetX = 30;
+    clipOffsetY = 0;
+  }
+  if (direction === 'left') {
+    clipWidth = 2;
+    clipHeight = 10;
+    clipOffsetX = 0;
+    clipOffsetY = 0;
+  }
+  if (direction === 'up') {
+    clipWidth = 10;
+    clipHeight = 2;
+    clipOffsetX = 0;
+    clipOffsetY = 0;
+  }
+  if (direction === 'down') {
+    clipWidth = 10;
+    clipHeight = 2;
+    clipOffsetX = 0;
+    clipOffsetY = 30;
+  }
 
-		var clipLength = clipWidth*clipHeight;
-    var whatColor = ctx.getImageData(guy.x+clipOffsetX, guy.y+clipOffsetY, clipWidth, clipHeight);
+  var clipLength = clipWidth * clipHeight;
+  var whatColor = ctx.getImageData(guy.x + clipOffsetX, guy.y + clipOffsetY,
+    clipWidth, clipHeight);
 
-      for (var i = 0; i < clipLength*4; i+=4 ) {
-        if((whatColor.data[i]===255)
-						&&
-						(whatColor.data[i+1]===0)
-						&&
-						(whatColor.data[i+2]===0)
-					){
-          console.log('red!');
-					return true;
-        };
-      }
+  for (var i = 0; i < clipLength * 4; i += 4) {
+    if ((whatColor.data[i] === 255) &&
+      (whatColor.data[i + 1] === 0) &&
+      (whatColor.data[i + 2] === 0)
+    ) {
+      console.log('red!');
+      return true;
+    };
+  }
 }
 
 
-function populateBoardWithTrueForRed(){
-	var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	var data = imageData.data;
-	for(i=0; i<data.length;i+=4){
-			if (data[i]>250 &&
-        (data[i+1]<10 && data[i+2]<10)){
-			currentBoard[i] = true;
-      currentBoard[i+1] = false;
-      currentBoard[i+2] = false;
-      currentBoard[i+3] = "taco";
-		} else {
-			currentBoard[i] = false;
-			currentBoard[i+1] = false;
-			currentBoard[i+2] = false;
-			currentBoard[i+3] = "burrito";
-		}
-	};
+function populateBoardWithTrueForRed() {
+  var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  var data = imageData.data;
+  for (i = 0; i < data.length; i += 4) {
+    if (data[i] > 250 &&
+      (data[i + 1] < 10 && data[i + 2] < 10)) {
+      currentBoard[i] = true;
+      currentBoard[i + 1] = false;
+      currentBoard[i + 2] = false;
+      currentBoard[i + 3] = "taco";
+    } else {
+      currentBoard[i] = false;
+      currentBoard[i + 1] = false;
+      currentBoard[i + 2] = false;
+      currentBoard[i + 3] = "burrito";
+    }
+  };
 };
 
-function isItAWall(x, y){
-	var x = Math.floor(x);
-	var y = Math.floor(y);
-	var isItRed = currentBoard[convertCoordinatesToArrayIndex(x,y)];
+function isItAWall(x, y) {
+  var x = Math.floor(x);
+  var y = Math.floor(y);
+  var isItRed = currentBoard[convertCoordinatesToArrayIndex(x, y)];
   console.log('red', isItRed);
-	if(isItRed===true){
-		return true;
-	} else {
-		return false;
-	};
+  if (isItRed === true) {
+    return true;
+  } else {
+    return false;
+  };
 };
 
 //================for testing==============================
@@ -174,38 +178,36 @@ var imageDataTester = ctx.getImageData(0, 0, canvas.width, canvas.height);
 var dataTest = imageDataTester.data;
 
 function convertCoordinatesToArrayIndex(x, y) {
-	return Math.floor(x)*(canvas.width*4)+Math.floor(y)*4
+  return Math.floor(x) * (canvas.width * 4) + Math.floor(y) * 4
 };
 
-function arrayChecker(array1, array2){
-	if (array1.length ===array2.length){
-		console.log('nice');
-	};
-	for (i = 0; i<array1.length; i++){
-		if(array1[i]===255 && array2[i]!=true){
-			console.log("aaaaaah!!!");
-		}
-	}
-	console.log('dun');
+function arrayChecker(array1, array2) {
+  if (array1.length === array2.length) {
+    console.log('nice');
+  };
+  for (i = 0; i < array1.length; i++) {
+    if (array1[i] === 255 && array2[i] != true) {
+      console.log("aaaaaah!!!");
+    }
+  }
+  console.log('dun');
 };
 
-function currentLevelArrayChecker(){
-  arrayChecker(ctx.getImageData(0, 0, canvas.width, canvas.height).data, currentBoard);
+function currentLevelArrayChecker() {
+  arrayChecker(ctx.getImageData(0, 0, canvas.width, canvas.height).data,
+    currentBoard);
 }
-
-
-
 
 //==============setting up keyboard============
 
 var keysDown = {};
 
-addEventListener("keydown", function (e) {
-	keysDown[e.keyCode] = true;
+addEventListener("keydown", function(e) {
+  keysDown[e.keyCode] = true;
 }, false);
 
-addEventListener("keyup", function (e) {
-	delete keysDown[e.keyCode];
+addEventListener("keyup", function(e) {
+  delete keysDown[e.keyCode];
 }, false);
 
 //---------------------------------------------------------------------------
@@ -216,19 +218,18 @@ addEventListener("keyup", function (e) {
 
 //==========get mouse coordinates (for testing) ===========
 
-function logMouseCoordinates(e){
-    var mouseX, mouseY;
+function logMouseCoordinates(e) {
+  var mouseX, mouseY;
 
-    if(e.offsetX) {
-        mouseX = e.offsetX;
-        mouseY = e.offsetY;
-    }
-    else if(e.layerX) {
-        mouseX = e.layerX;
-        mouseY = e.layerY;
-    }
-		console.log("x:" + mouseX);
-		console.log("y:" + mouseY);
+  if (e.offsetX) {
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
+  } else if (e.layerX) {
+    mouseX = e.layerX;
+    mouseY = e.layerY;
+  }
+  console.log("x:" + mouseX);
+  console.log("y:" + mouseY);
 }
 
 $('#canvas').on('click', logMouseCoordinates);
@@ -250,7 +251,7 @@ $('#canvas').on('click', logMouseCoordinates);
 //===========================================================================
 //---------------------------------------------------------------------------
 
-var update = function (modifier) {
+var update = function(modifier) {
 
   guy.update(modifier);
   bat.update(modifier);
@@ -274,54 +275,54 @@ var update = function (modifier) {
 
 //================ render things =====================
 
-var render = function () {
+var render = function() {
 
-	if (bgs[currentLevel]) {
-		ctx.drawImage(bgs[currentLevel].image, 0, 0);
-	}
+  if (bgs[currentLevel]) {
+    ctx.drawImage(bgs[currentLevel].image, 0, 0);
+  }
 
-	if(currentLevel===0){
-		ctx.drawImage(houseImage, 10, 10);
-	}
+  if (currentLevel === 0) {
+    ctx.drawImage(houseImage, 10, 10);
+  }
 
-	if (stairsReady){
-		ctx.drawImage(stairsImage, stairs.x, stairs.y);
-	}
+  if (stairsReady) {
+    ctx.drawImage(stairsImage, stairs.x, stairs.y);
+  }
 
-	if (coinReady&&currentLevel===1){
-		ctx.drawImage(coinImage, coin.x, coin.y)
-	}
+  if (coinReady && currentLevel === 1) {
+    ctx.drawImage(coinImage, coin.x, coin.y)
+  }
 
-	if (gemReady&&currentLevel===2){
-		ctx.drawImage(gemImage, gem.x, gem.y)
-	}
+  if (gemReady && currentLevel === 2) {
+    ctx.drawImage(gemImage, gem.x, gem.y)
+  }
 
-  if (swordReady&&currentLevel===1){
+  if (swordReady && currentLevel === 1) {
     ctx.drawImage(swordImage, sword.x, sword.y)
   }
 
-	if (ogreReady&&currentLevel===1){
-		ctx.drawImage(ogreImage, ogre.x, ogre.y)
-	}
+  if (ogreReady && currentLevel === 1) {
+    ctx.drawImage(ogreImage, ogre.x, ogre.y)
+  }
 
-  if(batReady&&currentLevel===2){
+  if (batReady && currentLevel === 2) {
     ctx.drawImage(batImage, bat.x, bat.y);
   }
 
-	if (guyReady){
-		ctx.drawImage(guyImage, guy.x+guy.offsetX, guy.y+guy.offsetY);
-	}
+  if (guyReady) {
+    ctx.drawImage(guyImage, guy.x + guy.offsetX, guy.y + guy.offsetY);
+  }
 
   // drawFog();
 
-  if(deadReady===true&&guy.health<0){
+  if (deadReady === true && guy.health < 0) {
     ctx.drawImage(deadImage, 0, 0);
     setTimeout(showControls, 2000);
-    };
+  };
 
 
-  if(powerOn===false){
-      ctx.fillRect(10,10,480,480);
+  if (powerOn === false) {
+    ctx.fillRect(10, 10, 480, 480);
   }
 
 };
@@ -332,16 +333,16 @@ var render = function () {
 //===========================================================================
 //---------------------------------------------------------------------------
 
-var main = function () {
-	var now = Date.now();
-	var delta = now - then;
+var main = function() {
+  var now = Date.now();
+  var delta = now - then;
 
-	update(delta / 1000);
-	render();
+  update(delta / 1000);
+  render();
 
-	then = now;
+  then = now;
 
-	requestAnimationFrame(main);
+  requestAnimationFrame(main);
 };
 
 //------------------------------------------------
@@ -349,9 +350,9 @@ var main = function () {
 // Cross-browser support for requestAnimationFrame
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame ||
-												w.webkitRequestAnimationFrame ||
-												w.msRequestAnimationFrame ||
-												w.mozRequestAnimationFrame;
+  w.webkitRequestAnimationFrame ||
+  w.msRequestAnimationFrame ||
+  w.mozRequestAnimationFrame;
 
 
 
